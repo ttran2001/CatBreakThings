fcaae8b208cf9957ef0263fc1e3fb1478902b27c
Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>
2023-03-24 16:01:54 -0500

    Update TutorialMenu.cs

--- CatBreakThings_v1/Assets/Scripts/TutorialMenu.cs
+++ CatBreakThings_v1/Assets/Scripts/TutorialMenu.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class TutorialMenu : MonoBehaviour
 {
     public void TutorialButton(){
-        SceneManager.LoadScene("Start Screen 1");    
+        SceneManager.LoadScene("Start Screen 1"); // Goes back to the start menu once user click the "Back to Menu" button in the tutorial scene
     }
 }
