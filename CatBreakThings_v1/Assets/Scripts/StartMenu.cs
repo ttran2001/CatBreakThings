b9f1c6a0dd7af52ba1ae0a52edc620b5f17b41a8
Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>
2023-03-24 16:00:38 -0500

    Update StartMenu.cs

--- CatBreakThings_v1/Assets/Scripts/StartMenu.cs
+++ CatBreakThings_v1/Assets/Scripts/StartMenu.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
     public void StartGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); // Load the first level screen when user hits the Start Menu button
     }
 }
