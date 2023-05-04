51ba7c1a64e693fdffe4031ef93234586ff3cdfb
Tyler Tuan Tran <tylertran161@gmail.com>
2023-05-04 01:40:44 -0500

    changes

--- CatBreakThings_v1/Assets/Scripts/Cat.cs
+++ CatBreakThings_v1/Assets/Scripts/Cat.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Cat : MonoBehaviour
 {
     public static float bottomY = -3.369995f;
+    public static bool dead = false; 
 
     void Update (){
         if(transform.position.y < bottomY){
+            dead = true;
             Destroy(this.gameObject); 
         }
     }
